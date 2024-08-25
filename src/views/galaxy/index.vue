97616b3dc9a1c9c85b931f856d9566e6f931c6e4
fox <1842208351@qq.com>
2024-08-25 20:15:47 +0800

    星球搭建

--- src/views/galaxy/index.vue
+++ src/views/galaxy/index.vue
@@ -1,27 +1,35 @@
 <template>
     <div class="galaxy">
-        <TresCanvas clear-color="black">
+        <TresCanvas clear-color="#005F5B">
+            <!-- 配置 -->
             <TresPerspectiveCamera :position="[0, 2, cameraZ]" />
             <OrbitControls />
             <MouseParallax :factor="5" :ease="[3, 0.1]" />
             <TresAmbientLight />
             <TresDirectionalLight :position="[10, 10, 15]" />
+
+            <!-- 背景 -->
             <Stars :rotation="[0, yRotation, 0]" :radius="5" :depth="50" :count="1000" :size="0.3"
                 :size-attenuation="true" />
 
-            <TresMesh :position="[0, 0, 0]">
-                <Sphere>
-                    <MeshGlassMaterial />
+            <!-- 星球 -->
+            <TresMesh class="planets" v-for="planet in planets" :key="planet.id" :position="[planet.x, planet.y, planet.z]">
+                <Sphere color="blue">
+                    <MeshGlassMaterial color="white" />
+                    <Sparkles :directional-light="lightRef" />
                 </Sphere>
             </TresMesh>
-            <Circle ref="circleRef" :args="[1, 1]" :position="[0, 0, 0]">
-                <TresMeshToonMaterial color="#33A8FF" />
-            </Circle>
+            <TresMesh class="planets" v-for="planet in planets" :key="planet.id" :position="[planet.x, planet.y, planet.z]">
+                <Circle :args="[1, 1]">
+                    <TresMeshToonMaterial color="#33A8FF" />
+                </Circle>
+            </TresMesh>
+
+
+
 
 
         </TresCanvas>
-
-
     </div>
 </template>
 
@@ -35,7 +43,9 @@ import {
     MeshWobbleMaterial,
     Sphere,
     Circle,
-    MouseParallax
+    MouseParallax,
+    CustomShaderMaterial,
+    Sparkles
 } from '@tresjs/cientos'
 
 const yRotation = shallowRef(0)
@@ -43,13 +53,17 @@ useRenderLoop().onLoop(({ delta }) => {
     yRotation.value += 0.02 * delta
 })
 
-const radius = ref(0)
-onMounted(() => {
 
+const lightRef = shallowRef()
+
+useRenderLoop().onLoop(({ elapsed }) => {
+    if (lightRef.value) {
+        lightRef.value.position.x = Math.cos(elapsed) * 2.5
+        lightRef.value.position.y = Math.sin(elapsed) * 2.5
+    }
 })
 
-const cameraZ = ref(2000); // Starting position (far)
-
+const cameraZ = ref(2000); // camera startPos
 const animateCamera = () => {
     if (cameraZ.value > 13) {
         cameraZ.value -= 7; //speed
@@ -60,6 +74,11 @@ const animateCamera = () => {
 onMounted(() => {
     requestAnimationFrame(animateCamera);
 });
+
+const planets = ref([
+    { x: -10, y: -3, z: -10, id: 0 },
+    { x: 3, y: -1, z: 0, id: 1 },
+])
 
 </script>
 
